d3a98a31c3d3008011464734c98da069da3f77a8
Nicolas Dumazet <nicdumz.commits@gmail.com>
2010-12-23 16:08:56 +0900

    add blog configuration

--- _config.py
+++ _config.py
@@ -1,3 +1,108 @@
+import logging
+logger = logging.getLogger("blogofile.config")
+
+######################################################################
+# Basic Settings
+#  (almost all sites will want to configure these settings)
+######################################################################
+## site_url -- Your site's full URL
+# Your "site" is the same thing as your _site directory.
+#  If you're hosting a blogofile powered site as a subdirectory of a larger
+#  non-blogofile site, then you would set the site_url to the full URL
+#  including that subdirectory: "http://www.yoursite.com/path/to/blogofile-dir"
+site.url         = "http://nicdumz.fr"
+
+#### Blog Settings ####
+blog = controllers.blog
+
+blog.enabled = True
+
+blog.path = "/blog"
+
+blog.name        = "Blog"
+
+## blog_description -- A short one line description of the blog
+# used in the RSS/Atom feeds.
+blog.description = "NicDumZ's developer blog"
+
+## blog_timezone -- the timezone that you normally write your blog posts from
+blog.timezone    = "Japan"
+
+## blog_posts_per_page -- Blog posts per page
+blog.posts_per_page = 5
+
+# Automatic Permalink
+# (If permalink is not defined in post article, it's generated
+#  automatically based on the following format:)
+# Available string replacements:
+# :year, :month, :day -> post's date
+# :title              -> post's title
+# :uuid               -> sha hash based on title
+# :filename           -> article's filename without suffix
+blog.auto_permalink.enabled = True
+# This is relative to site_url
+blog.auto_permalink.path    = "/blog/:year/:month/:day/:title"
+
+######################################################################
+# Intermediate Settings
+######################################################################
+#### Disqus.com comment integration ####
+blog.disqus.enabled = True
+blog.disqus.name    = "nicdumz"
+
+### Syntax highlighter ###
+# You can change the style to any builtin Pygments style
+# or, make your own: http://pygments.org/docs/styles
+filters.syntax_highlight.style   = "monokai"
+filters.syntax_highlight.css_dir = "/css"
+filters.syntax_highlight.preload_styles = ["emacs", "murphy","monokai"]
+#### Custom blog index ####
+# If you want to create your own index page at your blog root
+# turn this on. Otherwise blogofile assumes you want the
+# first X posts displayed instead
+blog.custom_index = False
+
+#Post excerpts
+#If you want to generate excerpts of your posts in addition to the
+#full post content turn this feature on
+post_excerpt_enabled     = False
+post_excerpt_word_length = 25
+#Also, if you don't like the way the post excerpt is generated
+#You can define a new function
+#below called post_excerpt(content, num_words)
+
+#### Blog pagination directory ####
+# blogofile places extra pages of your blog or category in
+# a secondary directory like the following:
+# http://www.yourblog.com/blog_root/page/4
+# http://www.yourblog.com/blog_root/category_1/page/4
+# You can rename the "page" part here:
+blog.pagination_dir = "page"
+
+#### Blog category directory ####
+# blogofile places extra pages of your or categories in
+# a secondary directory like the following:
+# http://www.yourblog.com/blog_root/category/your-topic/4
+# You can rename the "category" part here:
+blog.category_dir = "category"
+
+######################################################################
+# Advanced Settings
+######################################################################
+
+#### Default post filters ####
+# If a post does not specify a filter chain, use the 
+# following defaults based on the post file extension:
+blog.post_default_filters = {
+    "markdown": "syntax_highlight, markdown, paragraph_permalinks",
+    "textile": "syntax_highlight, textile, paragraph_permalinks",
+    "org": "syntax_highlight, org, paragraph_permalinks",
+    "rst": "syntax_highlight, rst, paragraph_permalinks",
+    "html": "syntax_highlight, paragraph_permalinks"
+}
+
+
+
 site.file_ignore_patterns += [
   ".*/LICENSE.txt",
   "^svgs/",
