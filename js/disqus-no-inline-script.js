f9397f3a4620ae8f824c4a1d569385e88107d1a8
Nicolas Dumazet <nicdumz.commits@gmail.com>
2015-06-27 14:56:50 +0200

    Fix urls when relevant.

--- js/disqus-no-inline-script.js
+++ js/disqus-no-inline-script.js
@@ -1,7 +1,13 @@
  $(function() {
      var bookmark = $("a[rel=bookmark]").get(0);
      var disqus_url   = bookmark.href;
-     var disqus_title = bookmark.title;
+     if (document.location.host.indexOf('localhost') !== -1) {
+         disqus_url = disqus_url.replace(document.location.host, 'nicdumz.fr');
+     }
+     if (disqus_url[disqus_url.length - 1] == '/') {
+        disqus_url = disqus_url.substr(0, disqus_url.length - 1);
+     }
+     var disqus_title = bookmark.text;
 
 
         var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
